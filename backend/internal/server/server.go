fc5a014e747e5f39de8c3acd06118b39550d7cdf
prachii06 <prachigohil201@gmail.com>
2026-07-18 19:09:44 +0530

    feat: add transaction service

--- backend/internal/server/server.go
+++ backend/internal/server/server.go
@@ -5,6 +5,7 @@ import(
 	"github.com/gin-gonic/gin"
 	"github.com/prachii06/LedgerX/internal/handlers"
 	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/prachii06/LedgerX/internal/services"
 )
 
 func New(port string, db *pgxpool.Pool) *gin.Engine{
@@ -15,6 +16,7 @@ func New(port string, db *pgxpool.Pool) *gin.Engine{
 	router.GET("/health", healthHandler.Health)
 	router.GET("/ready", healthHandler.Ready)
 	router.GET("/live", healthHandler.Live)
+	router.POST("/transactions", handlers.NewTransactionHandler(db).CreateTransaction)
 	
 	fmt.Printf("server running on port %s\n",port)
 
---
+++ backend/internal/services/transaction_services.go
@@ -0,0 +1,52 @@
+package services
+
+import (
+	"context"
+	"errors"
+	"time"
+	"github.com/google/uuid"
+	"github.com/prachii06/LedgerX/internal/models"
+	"github.com/prachii06/LedgerX/internal/repository"
+)
+
+type TransactionService struct {
+	repo *repository.TransactionRepository
+}
+
+func NewTransactionService(repo *repository.TransactionRepository) *TransactionService {
+	return &TransactionService{
+		repo: repo,
+	}
+}
+
+func (s *TransactionService) CreateTransaction(
+	ctx context.Context,
+	transaction *models.Transaction,
+) error {
+
+	if transaction.ExternalID == "" {
+		return errors.New("external_id is required")
+	}
+
+	if transaction.Amount <= 0 {
+		return errors.New("amount must be greater than zero")
+	}
+
+	if transaction.Currency == "" {
+		return errors.New("currency is required")
+	}
+
+	existing, err := s.repo.FindByExternalID(ctx, transaction.ExternalID)
+
+	if err == nil && existing != nil {
+		return errors.New("transaction already exists")
+	}
+
+	transaction.ID = uuid.New().String()
+	transaction.Status = models.StatusPending
+	transaction.CreatedAt = time.Now()
+	transaction.UpdatedAt = time.Now()
+
+
+	return s.repo.Create(ctx, transaction)
+}
